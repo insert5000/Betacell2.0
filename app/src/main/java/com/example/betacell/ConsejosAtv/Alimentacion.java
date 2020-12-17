003c4e128cc4feae67506bf6d5b792c3e53b61cf
insert5000 <derherreraa@upt.pe>
2020-12-16 21:26:50 -0500

    Commit Consejos2

---
+++ app/src/main/java/com/example/betacell/ConsejosAtv/Alimentacion.java
@@ -0,0 +1,16 @@
+package com.example.betacell.ConsejosAtv;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.betacell.R;
+
+public class Alimentacion extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_alimentacion);
+
+    }
+}
---
+++ app/src/main/java/com/example/betacell/ConsejosAtv/Riesgo.java
@@ -0,0 +1,17 @@
+package com.example.betacell.ConsejosAtv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.example.betacell.R;
+
+public class Riesgo extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riesgo);
+    }
+}
